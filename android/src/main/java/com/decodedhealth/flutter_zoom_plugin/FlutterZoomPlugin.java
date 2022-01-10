da641977c897ea88df17038ca475998321aa22e8
Ahmad Nashihuddien <ahmad.nshdn@gmail.com>
2022-01-10 16:43:40 +0700

    android V2 embedding

--- android/src/main/java/com/decodedhealth/flutter_zoom_plugin/FlutterZoomPlugin.java
+++ android/src/main/java/com/decodedhealth/flutter_zoom_plugin/FlutterZoomPlugin.java
@@ -1,12 +1,26 @@
 package com.decodedhealth.flutter_zoom_plugin;
 
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import androidx.annotation.NonNull;
 
 /** FlutterZoomPlugin */
 public class FlutterZoomPlugin {
 
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
+  /**public static void registerWith(Registrar registrar) {
     registrar.platformViewRegistry().registerViewFactory("flutter_zoom_plugin", new ZoomViewFactory(registrar.messenger()));
+  } */
+  
+  
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    this.flutterPluginBinding = binding;
   }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    this.flutterPluginBinding = null;
+  }
+
 }
